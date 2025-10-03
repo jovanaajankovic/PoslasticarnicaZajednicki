79898aab444740dc89e39561ab484517b68aae99
Jovana Jankovic <jankovicj297@gmail.com>
2025-10-04 01:59:36 +0200

    Dokumentovana klasa Racun

--- src/main/java/rs/ac/bg/fon/poslasticarnica/Racun.java
+++ src/main/java/rs/ac/bg/fon/poslasticarnica/Racun.java
@@ -6,168 +6,250 @@ import java.util.ArrayList;
 import java.util.Date;
 import java.sql.Timestamp;
 
-public class Racun extends AbstractDomainObject{
-	
-	  	private static final long serialVersionUID = 5L;
-	  
-	  	private Long racunID;
-	    private Date datumVreme;
-	    private double cena;
-	    private Administrator administrator;
-	    private ArrayList<StavkaRacuna> stavkeRacuna;
+/**
+ * Predstavlja racun koji se izdaje u poslasticarnici.
+ * <p>
+ * Racun sadrzi jedinstveni ID, datum i vreme izdavanja, ukupnu cenu,
+ * administratora koji ga je izdao i listu stavki racuna.
+ * </p>
+ *
+ * Svaki racun mora imati bar jednu stavku i mora biti vezan za postojeceg
+ * administratora.
+ * 
+ * Nasledjuje klasu AbstractDomainObject i implementira metode za perzistenciju
+ * u bazu podataka.
+ *
+ * @author Jovana Jankovic
+ */
+public class Racun extends AbstractDomainObject {
 
-	    public Racun(Long racunID, Date datumVreme, double cena, Administrator administrator, ArrayList<StavkaRacuna> stavkeRacuna) {
-	    	 setRacunID(racunID);
-			 setDatumVreme(datumVreme);
-			 setCena(cena);
-			 setAdministrator(administrator);
-			 setStavkeRacuna(stavkeRacuna);
-	    }
+	/** Jedinstveni identifikator verzije klase za potrebe serijalizacije. */
+	private static final long serialVersionUID = 5L;
 
-	    
-	    public Racun() {
-	    }
-	    
+	/** ID racuna kao Long */
+	private Long racunID;
 
-	    @Override
-	    public String nazivTabele() {
-	        return " Racun ";
-	    }
-	    
+	/** Datum i vreme izdavanja racuna kao Date */
+	private Date datumVreme;
 
-	    @Override
-	    public String alijas() {
-	        return " r ";
-	    }
+	/** Ukupna cena na racunu kao double */
+	private double cena;
 
-	    
-	    @Override
-	    public String join() {
-	        return " JOIN ADMINISTRATOR A ON (A.ADMINISTRATORID = R.ADMINISTRATORID) ";
-	    }
+	/** Administrator koji je izdao racun */
+	private Administrator administrator;
 
-	    
-	    @Override
-	    public ArrayList<AbstractDomainObject> vratiListu(ResultSet rs) throws SQLException {
-	        ArrayList<AbstractDomainObject> lista = new ArrayList<>();
+	/** Lista stavki racuna */
+	private ArrayList<StavkaRacuna> stavkeRacuna;
 
-	        while (rs.next()) {
-	            Administrator a = new Administrator(rs.getLong("AdministratorID"),
-	                    rs.getString("Ime"), rs.getString("Prezime"),
-	                    rs.getString("Username"), rs.getString("Password"));
+	/**
+	 * Inicijalizuje objekat klase Racun sa svim parametrima.
+	 *
+	 * @param racunID       ID racuna.Mora biti pozitivan i ne sme biti null.
+	 * @param datumVreme    Datum i vreme izdavanja. Ne sme biti null niti posle
+	 *                      trenutnog datuma.
+	 * @param cena          Ukupna cena racuna. Mora biti veca od nule.
+	 * @param administrator Administrator koji izdaje racun. Ne sme biti null.
+	 * @param stavkeRacuna  Lista stavki racuna. Ne sme biti null niti prazna.
+	 */
+	public Racun(Long racunID, Date datumVreme, double cena, Administrator administrator,
+			ArrayList<StavkaRacuna> stavkeRacuna) {
+		setRacunID(racunID);
+		setDatumVreme(datumVreme);
+		setCena(cena);
+		setAdministrator(administrator);
+		setStavkeRacuna(stavkeRacuna);
+	}
 
-	            Racun r = new Racun(rs.getLong("racunID"), rs.getTimestamp("datumVreme"),
-	                    rs.getDouble("cena"), a, null);
+	/**
+	 * Inicijalizuje objekat klase Racun sa atributima koji imaju default vrednosti.
+	 */
+	public Racun() {
+	}
 
-	            lista.add(r);
-	        }
+	@Override
+	public String nazivTabele() {
+		return " Racun ";
+	}
 
-	        rs.close();
-	        return lista;
-	    }
+	@Override
+	public String alijas() {
+		return " r ";
+	}
 
-	    
-	    @Override
-	    public String koloneZaInsert() {
-	        return " (datumVreme, cena, AdministratorID) ";
-	    }
+	@Override
+	public String join() {
+		return " JOIN ADMINISTRATOR A ON (A.ADMINISTRATORID = R.ADMINISTRATORID) ";
+	}
 
-	    
-	    @Override
-	    public String uslov() {
-	        return "";
-	    }
+	@Override
+	public ArrayList<AbstractDomainObject> vratiListu(ResultSet rs) throws SQLException {
+		ArrayList<AbstractDomainObject> lista = new ArrayList<>();
 
-	    
-	    @Override
-	    public String vrednostiZaInsert() {
-	        return "'" + new Timestamp(datumVreme.getTime()) + "', " + cena + ", "
-	                + " " + administrator.getAdministratorID() + " ";
-	    }
-	    
+		while (rs.next()) {
+			Administrator a = new Administrator(rs.getLong("AdministratorID"), rs.getString("Ime"),
+					rs.getString("Prezime"), rs.getString("Username"), rs.getString("Password"));
 
-	    @Override
-	    public String vrednostiZaUpdate() {
-	        return "";
-	    }
+			Racun r = new Racun(rs.getLong("racunID"), rs.getTimestamp("datumVreme"), rs.getDouble("cena"), a, null);
 
-	    
-	    @Override
-	    public String uslovZaSelect() {
-	        if (administrator != null) {
-	            return " WHERE A.ADMINISTRATORID = " + administrator.getAdministratorID();
-	        }
-	        return "";
-	    }
-	    
+			lista.add(r);
+		}
 
-	    public Long getRacunID() {
-	        return racunID;
-	    }
-	    
-	    public void setRacunID(Long racunID) {
-	    	 if (racunID == null)
-					throw new NullPointerException("ID ne sme biti null.");
-				
-			 if (racunID <= 0)
-			        throw new IllegalArgumentException("ID mora biti pozitivan broj.");
-			 
-	        this.racunID = racunID;
-	    }
-	    
+		rs.close();
+		return lista;
+	}
 
-	    public Date getDatumVreme() {
-	        return datumVreme;
-	    }
-	    
-	    public void setDatumVreme(Date datumVreme) {
-	    	if (datumVreme == null) 
-		        throw new NullPointerException("Datum i vreme ne mogu biti null.");
-		  
-	    	Date danas = new Date();
-	    	if (datumVreme.after(danas))
-		        throw new IllegalArgumentException("Datum ne moze biti u buducnosti.");
-	    	
-	        this.datumVreme = datumVreme;
-	    }
+	@Override
+	public String koloneZaInsert() {
+		return " (datumVreme, cena, AdministratorID) ";
+	}
 
-	    
-	    public double getCena() {
-	        return cena;
-	    }
+	@Override
+	public String uslov() {
+		return "";
+	}
 
-	    public void setCena(double cena) {
-	    	if (cena <= 0)
-			    throw new IllegalArgumentException("Cena mora biti veca od nule.");
-	    	
-	        this.cena = cena;
-	    }
+	@Override
+	public String vrednostiZaInsert() {
+		return "'" + new Timestamp(datumVreme.getTime()) + "', " + cena + ", " + " "
+				+ administrator.getAdministratorID() + " ";
+	}
 
-	    
-	    public Administrator getAdministrator() {
-	        return administrator;
-	    }
+	@Override
+	public String vrednostiZaUpdate() {
+		return "";
+	}
 
-	    public void setAdministrator(Administrator administrator) {
-			 if (administrator == null) 
-			     throw new NullPointerException("Racun mora imati administratora.");
-			 
-	        this.administrator = administrator;
-	    }
-	    
+	@Override
+	public String uslovZaSelect() {
+		if (administrator != null) {
+			return " WHERE A.ADMINISTRATORID = " + administrator.getAdministratorID();
+		}
+		return "";
+	}
 
-	    public ArrayList<StavkaRacuna> getStavkeRacuna() {
-	        return stavkeRacuna;
-	    }
+	/**
+	 * Vraca ID racuna.
+	 *
+	 * @return ID racuna
+	 */
+	public Long getRacunID() {
+		return racunID;
+	}
 
-	    public void setStavkeRacuna(ArrayList<StavkaRacuna> stavkeRacuna) {
-	    	 if (stavkeRacuna == null)
-					throw new NullPointerException("Lista stavki ne sme biti null.");
-				
-			 if (stavkeRacuna.isEmpty())
-					throw new IllegalArgumentException("Racun mora imati bar jednu stavku racuna.");
-				
-	        this.stavkeRacuna = stavkeRacuna;
-	    }
+	/**
+	 * Postavlja ID racuna. ID racuna mora biti pozitivan broj i ne sme biti null.
+	 *
+	 * @param racunID ID racuna
+	 * @throws java.lang.NullPointerException     ako je ID null
+	 * @throws java.lang.IllegalArgumentException ako ID nije pozitivan broj veci od
+	 *                                            nule
+	 */
+	public void setRacunID(Long racunID) {
+		if (racunID == null)
+			throw new NullPointerException("ID ne sme biti null.");
+
+		if (racunID <= 0)
+			throw new IllegalArgumentException("ID mora biti pozitivan broj.");
+
+		this.racunID = racunID;
+	}
+
+	/**
+	 * Vraca datum i vreme kada je racun izdat.
+	 *
+	 * @return datum i vreme racuna
+	 */
+	public Date getDatumVreme() {
+		return datumVreme;
+	}
+
+	/**
+	 * Postavlja datum i vreme izdavanja racuna. Datum i vreme ne smeju biti null
+	 * niti u buducnosti.
+	 *
+	 * @param datumVreme Datum i vreme izdavanja
+	 * @throws java.lang.NullPointerException     ako je datum null
+	 * @throws java.lang.IllegalArgumentException ako je datum u buducnosti
+	 */
+	public void setDatumVreme(Date datumVreme) {
+		if (datumVreme == null)
+			throw new NullPointerException("Datum i vreme ne mogu biti null.");
+
+		Date danas = new Date();
+		if (datumVreme.after(danas))
+			throw new IllegalArgumentException("Datum ne moze biti u buducnosti.");
+
+		this.datumVreme = datumVreme;
+	}
+
+	/**
+	 * Vraca ukupnu cenu racuna.
+	 *
+	 * @return ukupna cena
+	 */
+	public double getCena() {
+		return cena;
+	}
+
+	/**
+	 * Postavlja ukupnu cenu racuna. Cena mora biti veca od nule.
+	 *
+	 * @param cena Cena racuna
+	 * @throws java.lang.IllegalArgumentException ako je cena manja ili jednaka nuli
+	 */
+	public void setCena(double cena) {
+		if (cena <= 0)
+			throw new IllegalArgumentException("Cena mora biti veca od nule.");
+
+		this.cena = cena;
+	}
+
+	/**
+	 * Vraca administratora koji je izdao racun.
+	 *
+	 * @return administrator
+	 */
+	public Administrator getAdministrator() {
+		return administrator;
+	}
+
+	/**
+	 * Postavlja administratora koji je izdao racun. Administrator ne sme biti null.
+	 *
+	 * @param administrator Administrator koji je izdao racun
+	 * @throws java.lang.NullPointerException ako je administrator null
+	 */
+	public void setAdministrator(Administrator administrator) {
+		if (administrator == null)
+			throw new NullPointerException("Racun mora imati administratora.");
+
+		this.administrator = administrator;
+	}
+
+	/**
+	 * Vraca listu stavki na racunu.
+	 *
+	 * @return lista stavki
+	 */
+	public ArrayList<StavkaRacuna> getStavkeRacuna() {
+		return stavkeRacuna;
+	}
+
+	/**
+	 * Postavlja listu stavki na racunu. Lista stavki ne sme biti null niti prazna.
+	 *
+	 * @param stavkeRacuna Lista stavki
+	 * @throws java.lang.NullPointerException     ako je lista null
+	 * @throws java.lang.IllegalArgumentException ako je lista prazna
+	 */
+	public void setStavkeRacuna(ArrayList<StavkaRacuna> stavkeRacuna) {
+		if (stavkeRacuna == null)
+			throw new NullPointerException("Lista stavki ne sme biti null.");
+
+		if (stavkeRacuna.isEmpty())
+			throw new IllegalArgumentException("Racun mora imati bar jednu stavku racuna.");
+
+		this.stavkeRacuna = stavkeRacuna;
+	}
 
 }
