2cf843a2c7a02fe61d7b85c4938251caf2f43834
Jovana Jankovic <jankovicj297@gmail.com>
2025-10-04 02:06:08 +0200

    Dokumentovana klasa StavkaRacuna

--- src/main/java/rs/ac/bg/fon/poslasticarnica/StavkaRacuna.java
+++ src/main/java/rs/ac/bg/fon/poslasticarnica/StavkaRacuna.java
@@ -4,168 +4,236 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.util.ArrayList;
 
+/**
+ * Predstavlja stavku racuna u poslasticarnici. Svaka stavka pripada odredjenom
+ * racunu i sadrzi redni broj, kolicinu, cenu i poslasticu na koju se odnosi.
+ * 
+ * Nasledjuje klasu AbstractDomainObject i implementira metode za perzistenciju
+ * u bazu podataka.
+ *
+ * @author Jovana Jankovic
+ */
 public class StavkaRacuna extends AbstractDomainObject {
-	
-	    private static final long serialVersionUID = 7L;
-	
-	    private Racun racun;
-	    private int rb;
-	    private int kolicina;
-	    private double cena;
-	    private Poslastica poslastica;
 
-	    public StavkaRacuna(Racun racun, int rb, int kolicina, double cena, Poslastica poslastica) {
-	    	 setRacun(racun);
-			 setRb(rb);
-			 setKolicina(kolicina);
-			 setCena(cena);
-			 setPoslastica(poslastica);
-	    }
-	    
+	/** Jedinstveni identifikator verzije klase za potrebe serijalizacije. */
+	private static final long serialVersionUID = 7L;
 
-	    public StavkaRacuna() {
-	    }
-	    
+	/** Racun kome stavka pripada */
+	private Racun racun;
 
-	    @Override
-	    public String nazivTabele() {
-	        return " StavkaRacuna ";
-	    }
-	    
+	/** Redni broj stavke kao int */
+	private int rb;
 
-	    @Override
-	    public String alijas() {
-	        return " sr ";
-	    }
-	    
+	/** Kolicina stavke na racunu kao int */
+	private int kolicina;
 
-	    @Override
-	    public String join() {
-	        return " JOIN POSLASTICA P ON (P.POSLASTICAID = SR.POSLASTICAID) "
-	                + "JOIN TIPPOSLASTICE TP ON (TP.TIPPOSLASTICEID = P.TIPPOSLASTICEID) "
-	                + "JOIN RACUN R ON (R.RACUNID = SR.RACUNID) "
-	                + "JOIN ADMINISTRATOR A ON (A.ADMINISTRATORID = R.ADMINISTRATORID) ";
-	    }
-	    
+	/** Cena stavke kao double */
+	private double cena;
 
-	    @Override
-	    public ArrayList<AbstractDomainObject> vratiListu(ResultSet rs) throws SQLException {
-	        ArrayList<AbstractDomainObject> lista = new ArrayList<>();
+	/** Poslastica na koju se stavka odnosi */
+	private Poslastica poslastica;
 
-	        while (rs.next()) {
-	            Administrator a = new Administrator(rs.getLong("AdministratorID"),
-	                    rs.getString("Ime"), rs.getString("Prezime"),
-	                    rs.getString("Username"), rs.getString("Password"));
+	/**
+	 * Inicijalizuje objekat klase StavkaRacuna sa svim parametrima.
+	 *
+	 * @param racun      Racun kojem stavka pripada. Ne sme biti null.
+	 * @param rb         Redni broj stavke. Mora biti pozitivan broj.
+	 * @param kolicina   Kolicina poslastica. Mora biti veca od nule.
+	 * @param cena       Cena stavke. Mora biti veca od nule.
+	 * @param poslastica Poslastica koja se nalazi u stavci. Ne sme biti null.
+	 */
+	public StavkaRacuna(Racun racun, int rb, int kolicina, double cena, Poslastica poslastica) {
+		setRacun(racun);
+		setRb(rb);
+		setKolicina(kolicina);
+		setCena(cena);
+		setPoslastica(poslastica);
+	}
 
-	            Racun r = new Racun(rs.getLong("racunID"), rs.getTimestamp("datumVreme"),
-	                    rs.getDouble("r.cena"), a, null);
+	/**
+	 * Inicijalizuje objekat klase StavkaRacuna sa atributima koji imaju default
+	 * vrednosti.
+	 */
+	public StavkaRacuna() {
+	}
 
-	            TipPoslastice tp = new TipPoslastice(rs.getLong("TipPoslasticeID"),
-	                    rs.getString("tp.Naziv"));
+	@Override
+	public String nazivTabele() {
+		return " StavkaRacuna ";
+	}
 
-	            Poslastica p = new Poslastica(rs.getLong("poslasticaID"), rs.getString("p.naziv"),
-	                    rs.getDouble("cenaPoKomadu"), rs.getString("opis"), tp, null);
+	@Override
+	public String alijas() {
+		return " sr ";
+	}
 
-	            StavkaRacuna sr = new StavkaRacuna(r, rs.getInt("rb"), rs.getInt("kolicina"),
-	                    rs.getDouble("sr.cena"), p);
+	@Override
+	public String join() {
+		return " JOIN POSLASTICA P ON (P.POSLASTICAID = SR.POSLASTICAID) "
+				+ "JOIN TIPPOSLASTICE TP ON (TP.TIPPOSLASTICEID = P.TIPPOSLASTICEID) "
+				+ "JOIN RACUN R ON (R.RACUNID = SR.RACUNID) "
+				+ "JOIN ADMINISTRATOR A ON (A.ADMINISTRATORID = R.ADMINISTRATORID) ";
+	}
 
-	            lista.add(sr);
-	        }
+	@Override
+	public ArrayList<AbstractDomainObject> vratiListu(ResultSet rs) throws SQLException {
+		ArrayList<AbstractDomainObject> lista = new ArrayList<>();
 
-	        rs.close();
-	        return lista;
-	    }
-	    
+		while (rs.next()) {
+			Administrator a = new Administrator(rs.getLong("AdministratorID"), rs.getString("Ime"),
+					rs.getString("Prezime"), rs.getString("Username"), rs.getString("Password"));
 
-	    @Override
-	    public String koloneZaInsert() {
-	        return " (racunID, rb, kolicina, cena, poslasticaID) ";
-	    }
-	    
+			Racun r = new Racun(rs.getLong("racunID"), rs.getTimestamp("datumVreme"), rs.getDouble("r.cena"), a, null);
 
-	    @Override
-	    public String uslov() {
-	        return " racunID = " + racun.getRacunID() + " AND RB = " + rb;
-	    }
-	    
+			TipPoslastice tp = new TipPoslastice(rs.getLong("TipPoslasticeID"), rs.getString("tp.Naziv"));
 
-	    @Override
-	    public String vrednostiZaInsert() {
-	        return " " + racun.getRacunID() + ", " + rb + ", "
-	                + " " + kolicina + ", " + cena + ", " + poslastica.getPoslasticaID();
-	    }
-	    
+			Poslastica p = new Poslastica(rs.getLong("poslasticaID"), rs.getString("p.naziv"),
+					rs.getDouble("cenaPoKomadu"), rs.getString("opis"), tp, null);
 
-	    @Override
-	    public String vrednostiZaUpdate() {
-	        return "";
-	    }
-	    
+			StavkaRacuna sr = new StavkaRacuna(r, rs.getInt("rb"), rs.getInt("kolicina"), rs.getDouble("sr.cena"), p);
 
-	    @Override
-	    public String uslovZaSelect() {
-	        return " WHERE R.RACUNID = " + racun.getRacunID();
-	    }
-	    
+			lista.add(sr);
+		}
 
-	    public Racun getRacun() {
-	        return racun;
-	    }
+		rs.close();
+		return lista;
+	}
 
-	    public void setRacun(Racun racun) {
-	    	if (racun == null) 
-		        throw new NullPointerException("Racun ne sme biti null.");
-	    	
-	        this.racun = racun;
-	    }
+	@Override
+	public String koloneZaInsert() {
+		return " (racunID, rb, kolicina, cena, poslasticaID) ";
+	}
 
-	    
-	    public int getRb() {
-	        return rb;
-	    }
+	@Override
+	public String uslov() {
+		return " racunID = " + racun.getRacunID() + " AND RB = " + rb;
+	}
 
-	    public void setRb(int rb) {
-	    	if (rb <= 0)
-		        throw new IllegalArgumentException("Redni broj mora biti pozitivan broj.");
-	    	
-	        this.rb = rb;
-	    }
+	@Override
+	public String vrednostiZaInsert() {
+		return " " + racun.getRacunID() + ", " + rb + ", " + " " + kolicina + ", " + cena + ", "
+				+ poslastica.getPoslasticaID();
+	}
 
-	    
-	    public int getKolicina() {
-	        return kolicina;
-	    }
+	@Override
+	public String vrednostiZaUpdate() {
+		return "";
+	}
 
-	    public void setKolicina(int kolicina) {
-	    	if (kolicina <= 0)
-		        throw new IllegalArgumentException("Kolicina mora biti veca od nule.");
-	    	
-	        this.kolicina = kolicina;
-	    }
+	@Override
+	public String uslovZaSelect() {
+		return " WHERE R.RACUNID = " + racun.getRacunID();
+	}
 
-	    
-	    public double getCena() {
-	        return cena;
-	    }
+	/**
+	 * Vraca racun kome stavka pripada.
+	 *
+	 * @return racun stavke
+	 */
+	public Racun getRacun() {
+		return racun;
+	}
 
-	    public void setCena(double cena) {
-	    	if (cena <= 0)
-		        throw new IllegalArgumentException("Cena mora biti veca od nule.");
-	    	
-	        this.cena = cena;
-	    }
-	    
+	/**
+	 * Postavlja racun kome stavka pripada. Racun ne sme biti null.
+	 *
+	 * @param racun Racun kome stavka pripada
+	 * @throws java.lang.NullPointerException ako je racun null
+	 */
+	public void setRacun(Racun racun) {
+		if (racun == null)
+			throw new NullPointerException("Racun ne sme biti null.");
 
-	    public Poslastica getPoslastica() {
-	        return poslastica;
-	    }
+		this.racun = racun;
+	}
 
-	    public void setPoslastica(Poslastica poslastica) {
-	    	if (poslastica == null) 
-		        throw new NullPointerException("Poslastica ne sme biti null.");
-	    	
-	        this.poslastica = poslastica;
-	    }
+	/**
+	 * Vraca redni broj stavke.
+	 * 
+	 * @return redni broj
+	 */
+	public int getRb() {
+		return rb;
+	}
 
+	/**
+	 * Postavlja redni broj stavke. Redni broj mora biti pozitivan.
+	 *
+	 * @param rb Redni broj stavke
+	 * @throws java.lang.IllegalArgumentException ako redni broj nije pozitivan
+	 */
+	public void setRb(int rb) {
+		if (rb <= 0)
+			throw new IllegalArgumentException("Redni broj mora biti pozitivan broj.");
+
+		this.rb = rb;
+	}
+
+	/**
+	 * Vraca kolicinu poslastica u stavci.
+	 *
+	 * @return kolicina
+	 */
+	public int getKolicina() {
+		return kolicina;
+	}
+
+	/**
+	 * Postavlja kolicinu poslastica u stavci. Kolicina mora biti veca od nule.
+	 *
+	 * @param kolicina Kolicina poslastica
+	 * @throws java.lang.IllegalArgumentException ako je kolicina manja ili jednaka
+	 *                                            nuli
+	 */
+	public void setKolicina(int kolicina) {
+		if (kolicina <= 0)
+			throw new IllegalArgumentException("Kolicina mora biti veca od nule.");
+
+		this.kolicina = kolicina;
+	}
+
+	/**
+	 * Vraca cenu stavke racuna.
+	 *
+	 * @return cena stavke
+	 */
+	public double getCena() {
+		return cena;
+	}
+
+	/**
+	 * Postavlja cenu stavke racuna. Cena mora biti veca od nule.
+	 *
+	 * @param cena Cena stavke racuna
+	 * @throws java.lang.IllegalArgumentException ako je cena manja ili jednaka nuli
+	 */
+	public void setCena(double cena) {
+		if (cena <= 0)
+			throw new IllegalArgumentException("Cena mora biti veca od nule.");
+
+		this.cena = cena;
+	}
+
+	/**
+	 * Vraca poslasticu koja je deo stavke racuna.
+	 *
+	 * @return poslastica
+	 */
+	public Poslastica getPoslastica() {
+		return poslastica;
+	}
+
+	/**
+	 * Postavlja poslasticu za stavku racuna. Poslastica ne sme biti null.
+	 *
+	 * @param poslastica Poslastica
+	 * @throws java.lang.NullPointerException ako je poslastica null
+	 */
+	public void setPoslastica(Poslastica poslastica) {
+		if (poslastica == null)
+			throw new NullPointerException("Poslastica ne sme biti null.");
+
+		this.poslastica = poslastica;
+	}
 
 }
