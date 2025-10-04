2d7048020bd4dcf458ffb28a9195058fa4c55f35
Jovana Jankovic <jankovicj297@gmail.com>
2025-10-04 02:21:34 +0200

    Dokumentovana klasa Response

--- src/main/java/transfer/Response.java
+++ src/main/java/transfer/Response.java
@@ -4,46 +4,101 @@ import java.io.Serializable;
 
 import transfer.util.ResponseStatus;
 
+/**
+ * Klasa koja predstavlja odgovor servera na zahtev klijenta.
+ * <p>
+ * Odgovor moze sadrzati podatke, izuzetak (ako je doslo do greske) i status
+ * odgovora (uspeh ili greska). Implementira Serializable radi slanja preko
+ * mreze.
+ * 
+ * @author Jovana Jankovic
+ */
 public class Response implements Serializable {
-	
-    private static final long serialVersionUID = 2L;
-	
-	
+
+	/** Jedinstveni identifikator verzije klase za potrebe serijalizacije. */
+	private static final long serialVersionUID = 2L;
+
+	/** Podaci koje server vraca klijentu. */
 	private Object data;
-    private Exception exc;
-    private ResponseStatus responseStatus;
 
-    public Response() {
-    }
+	/** Izuzetak koji se desio prilikom izvrsavanja operacije (ako postoji). */
+	private Exception exc;
 
-    public Response(Object data, Exception exc, ResponseStatus responseStatus) {
-        this.data = data;
-        this.exc = exc;
-        this.responseStatus = responseStatus;
-    }
+	/** Status odgovora (uspesan ili neuspesan). */
+	private ResponseStatus responseStatus;
 
-    public Object getData() {
-        return data;
-    }
+	/**
+	 * Inicijalizuje objekat klase Response sa atributima koji imaju default
+	 * vrednosti.
+	 */
+	public Response() {
+	}
 
-    public void setData(Object data) {
-        this.data = data;
-    }
+	/**
+	 * Inicijalizuje objekat klase Response sa svim parametrima.
+	 * 
+	 * @param data           Podaci koje server vraca klijentu
+	 * @param exc            Izuzetak ako je doslo do greske
+	 * @param responseStatus Status odgovora (SUCCESS / ERROR)
+	 */
+	public Response(Object data, Exception exc, ResponseStatus responseStatus) {
+		this.data = data;
+		this.exc = exc;
+		this.responseStatus = responseStatus;
+	}
 
-    public Exception getException() {
-        return exc;
-    }
+	/**
+	 * Vraca podatke koje je server poslao.
+	 * 
+	 * @return objekat sa podacima
+	 */
+	public Object getData() {
+		return data;
+	}
 
-    public void setException(Exception exc) {
-        this.exc = exc;
-    }
+	/**
+	 * Postavlja podatke koje server salje.
+	 * 
+	 * @param data Objekat sa podacima
+	 */
+	public void setData(Object data) {
+		this.data = data;
+	}
 
-    public ResponseStatus getResponseStatus() {
-        return responseStatus;
-    }
+	/**
+	 * Vraca izuzetak ako je doslo do greske.
+	 * 
+	 * @return Exception ili null ako nema greske
+	 */
+	public Exception getException() {
+		return exc;
+	}
 
-    public void setResponseStatus(ResponseStatus responseStatus) {
-        this.responseStatus = responseStatus;
-    }
+	/**
+	 * Postavlja izuzetak koji se dogodio.
+	 * 
+	 * @param exc Instanca izuzetka
+	 */
+	public void setException(Exception exc) {
+		this.exc = exc;
+	}
+
+	/**
+	 * Vraca status odgovora.
+	 * 
+	 * @return SUCCESS ili ERROR
+	 */
+	public ResponseStatus getResponseStatus() {
+		return responseStatus;
+	}
+
+	/**
+	 * Postavlja status odgovora.
+	 * 
+	 * @param responseStatus SUCCESS ili ERROR
+	 */
+	public void setResponseStatus(ResponseStatus responseStatus) {
+		this.responseStatus = responseStatus;
+	}
 
 }
