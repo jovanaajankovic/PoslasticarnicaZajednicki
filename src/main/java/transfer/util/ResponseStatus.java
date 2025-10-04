c76415b6f4117f3d31a3572f8d454c2e5ebaa512
Jovana Jankovic <jankovicj297@gmail.com>
2025-10-04 02:25:09 +0200

    Dokumentovan enum ResponseStatus

--- src/main/java/transfer/util/ResponseStatus.java
+++ src/main/java/transfer/util/ResponseStatus.java
@@ -1,7 +1,19 @@
 package transfer.util;
 
+/**
+ * Enumeracija koja predstavlja status odgovora servera.
+ * <p>
+ * Koristi se u klasi Response kako bi se oznacilo da li je zahtev uspesno
+ * izvrsen ili je doslo do greske.
+ * 
+ * @author Jovana Jankovic
+ */
 public enum ResponseStatus {
-	
-	Success, Error
+
+	/** Oznaka da je operacija uspesno izvrsena. */
+	Success,
+
+	/** Oznaka da je doslo do greske pri izvrsavanju operacije. */
+	Error
 
 }
